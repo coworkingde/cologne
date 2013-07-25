cc010dd38c1acd574e5f029a751037f5ce97242e
Michael Bumann <michael@railslove.com>
2013-07-25 15:54:02 +0200

    hello world

--- _site/assets/space-1d196d0a526a0672f76b48e75b50bf33.js
+++ _site/assets/space-1d196d0a526a0672f76b48e75b50bf33.js
@@ -606,10 +606,20 @@ function isMobile() {
 }
 var map = L.map('map', {zoomControl: false, scrollWheelZoom: false}).setView([50.9222007, 6.9628545359], 14);
 
+var icon = L.icon({
+      iconUrl: "/img/marker/badge_pink.png",
+      shadowUrl: '/img/marker/shadow.png',
+      iconSize:     [47, 27],
+      shadowSize:   [77,37],
+      iconAnchor:   [23, 13],
+      shadowAnchor: [23, 12],
+      popupAnchor:  [0, -3]
+  })
+
 L.tileLayer.provider('MapBox.slogmen.map-64a4ugze').addTo(map);
 L.control.zoom({position: 'bottomleft'}).addTo(map);
 L.marker([50.9222007, 6.9628545359],
-  { bounceOnAdd: true, bounceOnAddOptions: {duration: 1000, height: 200}}
+  {icon: icon, bounceOnAdd: true, bounceOnAddOptions: {duration: 1200, height: 300}}
 ).addTo(map)
 .bindPopup('<b>Coworking Cologne</b> <br> <span>An der Bottmühle 5<br />50678 Köln');
 
